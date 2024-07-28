0ebec23392fa1107473912c18a14f0fb17a76a74
vishwa vijeta <vishwa@expertrec.com>
2024-07-28 21:20:37 +0530

    List of change:
    1. imports components.
    2. Added fucntionality to search
    3. Added functionality to expand and close. Also, stop this when edit is
       true
    4. Added functionality to delete and open dailog box
    5. calculate age and vice-versa to store dob when edits happens
    6. handle save

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import data from './utils/celebrities.json';
+import SearchBar from './components/SearchBar/SearchBar';
+import CelebtitiesList from './components/CelebritiesList/CelebritiesList';
+import ConfimationDialog from './components/ConfimationDialog/ConfimationDialog';
+import { calculateAge } from './utils/AgeCalculation';
+import { calculateDOB } from './utils/AgeCalculation';
 import './App.css';
 
-function App() {
+// declare the type for the celebtities
+type Celebtities = {
+  id: number;
+  first: string;
+  last: string;
+  dob: string;
+  gender: string;
+  email: string;
+  picture: string;
+  country: string;
+  description: string;
+};
+
+const App: React.FC = () => {
+
+  // to store the celebtities
+  const [celebtities, setCelebtities] = useState<Celebtities[]>([]);
+
+  // to store the index of the celebtity that is currently open
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  // to store the index of the celebtity that is currently being edited
+  const [allowEdit, setAllowEdit] = useState<number | null>(null);
+
+  // to store the index of the celebtity that is currently being deleted
+  const [deleteIndex, setDeleteIndex] = useState<number | null>(null);
+
+  // to store the state of the dialog
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+
+  // load the celebtities from the json file
+  useEffect(() => {
+    setCelebtities(data);
+  }, []);
+
+
+  // handle the expansion of the celebtity
+  const handleExapansion = (index: number) => {
+    if (allowEdit) return;
+    setOpenIndex(index === openIndex ? null : index);
+  };
+
+  // handle the edit of the celebtity
+  const handleEdit = (index: number) => {
+    setAllowEdit(index === allowEdit ? null : index);
+  };
+
+  // handle the delete of the celebtity
+  const handleDelete = (index: number) => {
+    const newCelebtities = [...celebtities];
+    newCelebtities.splice(index, 1);
+    setCelebtities(newCelebtities);
+  };
+
+  // handle the delete click on celebrity
+  const handleDeleteClick = (index: number) => {
+    setDeleteIndex(index);
+    setIsDialogOpen(true);
+  };
+
+  // handle the dialog close
+  const handleDialogClose = (action: 'cancel' | 'delete') => {
+    setIsDialogOpen(false);
+    if (action === 'delete' && deleteIndex !== null) {
+      handleDelete(deleteIndex);
+    }
+    setDeleteIndex(null);
+  };
+
+  // handle the save of the celebtity
+  const handleSave = (index: number, updatedCelebtity: any) => {
+    // update age to dob before saving
+    updatedCelebtity.dob = calculateDOB(updatedCelebtity.dob);
+    const newCelebtities = [...celebtities];
+    newCelebtities[index] = { ...newCelebtities[index], ...updatedCelebtity };
+    setCelebtities(newCelebtities);
+    setAllowEdit(null);
+  };
+
+  // handle the search of the celebtity
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value;
+    // search by first name and last name
+    const filteredCelebtities = data.filter((celebtity) => {
+      return celebtity.first.toLowerCase().includes(searchValue.toLowerCase()) ||
+        celebtity.last.toLowerCase().includes(searchValue.toLowerCase());
+    });
+    setCelebtities(filteredCelebtities);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="list-container">
+        < SearchBar handleSearch={handleSearch} />
+        {celebtities.map((celebtity, index) => (
+          <CelebtitiesList
+            key={index}
+            first={celebtity.first}
+            last={celebtity.last}
+            dob={calculateAge(celebtity.dob)}
+            gender={celebtity.gender}
+            country={celebtity.country}
+            picture={celebtity.picture}
+            description={celebtity.description}
+            isOpen={openIndex === index}
+            allowEdit={allowEdit === index}
+            handleExapansion={() => handleExapansion(index)}
+            onEdit={() => handleEdit(index)}
+            handleDelete={() => handleDeleteClick(index)}
+            onSave={(updatedCelebtity) => handleSave(index, updatedCelebtity)}
+          />
+        ))}
+      </div>
+      {isDialogOpen && <ConfimationDialog onClose={handleDialogClose} title="Are you sure you want to delete?" />}
+    </>
+  )
+};
 
 export default App;
